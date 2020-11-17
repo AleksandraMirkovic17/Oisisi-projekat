c54a6da7835c6dd2b205d02f537e7ddd09ddacd1
Aleksandra Mirkovic <aleksandramirkovic17@uns.ac.rs>
2020-11-17 01:24:01 +0100

    #profesor kreiranje

---
+++ src/gui/Profesor.java
@@ -0,0 +1,19 @@
+package gui;
+
+public class Profesor {
+	public String ime;
+	private String prezime;
+	private String datumRodjena;
+	private String brojTelefona;
+	private String adresaStanovanja;
+	private String email;
+	private String adresaKancelarije;
+	private int brLicneKarte;
+	private String titula;
+	private String zvanjeProfesora;
+	
+	
+	
+	
+	
+}
