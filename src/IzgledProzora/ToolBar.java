7b32e365842e4f75a3368336563e4581d438c6ea
Aleksandra Mirkovic <aleksandramirkovic17@uns.ac.rs>
2020-11-19 21:19:49 +0100

    #toolbar

--- src/IzgledProzora/ToolBar.java
+++ src/IzgledProzora/ToolBar.java
@@ -1,9 +1,15 @@
 package IzgledProzora;
 
+import java.awt.BorderLayout;
+import java.awt.Dimension;
+import java.awt.FlowLayout;
 import java.awt.Image;
 
+import javax.swing.Box;
+import javax.swing.BoxLayout;
 import javax.swing.ImageIcon;
 import javax.swing.JButton;
+import javax.swing.JTextField;
 import javax.swing.JToolBar;
 import javax.swing.SwingConstants;
 
@@ -34,7 +40,20 @@ public class ToolBar extends JToolBar  {
 			btnNew2.setIcon(imageIcon2);
 			add(btnNew2);
 			addSeparator();
-		
+			
+			JButton btnNew22 = new JButton();
+			btnNew22.setToolTipText("Delet");
+			ImageIcon imageIcon22 = new ImageIcon(new ImageIcon("Slike/lupa.jpg").getImage().getScaledInstance(22, 22, Image.SCALE_DEFAULT));
+			btnNew22.setIcon(imageIcon22);
+			add(btnNew22);
+			addSeparator();
+			
+			JTextField pretraga=new JTextField();
+			pretraga.setPreferredSize(new Dimension(50,30));
+			add(Box.createHorizontalStrut(700));
+			add(pretraga);
+			add(Box.createHorizontalStrut(10));
+			add(btnNew22);
+			addSeparator();
 		}
-
 }
