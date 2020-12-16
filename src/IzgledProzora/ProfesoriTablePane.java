30a3bbaa5556fdd29d667aa537fbb2229ee4bcd0
Aleksandra Mirkovic <aleksandramirkovic17@uns.ac.rs>
2020-12-16 16:07:10 +0100

    #glavni_prozor Dodavanje Tabova

---
+++ src/IzgledProzora/ProfesoriTablePane.java
@@ -0,0 +1,16 @@
+package IzgledProzora;
+
+import java.awt.BorderLayout;
+
+import javax.swing.JPanel;
+
+
+public class ProfesoriTablePane extends JPanel{
+ 
+	public ProfesoriTablePane()
+	{
+		setLayout(new BorderLayout());
+	}
+	
+	
+}
