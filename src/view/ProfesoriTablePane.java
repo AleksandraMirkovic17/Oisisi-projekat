65164918764dcfdc62d8c44d41352c5a6336bf46
AleksandraMirkovic <73426697+AleksandraMirkovic@users.noreply.github.com>
2021-01-05 10:15:53 +0100

    #sortiranje_profesora

--- src/view/ProfesoriTablePane.java
+++ src/view/ProfesoriTablePane.java
@@ -4,6 +4,8 @@ import java.awt.BorderLayout;
 
 import javax.swing.JPanel;
 import javax.swing.JScrollPane;
+import javax.swing.table.TableModel;
+import javax.swing.table.TableRowSorter;
 
 
 
@@ -23,6 +25,8 @@ public class ProfesoriTablePane extends JPanel{
 	private void prikazProfesora()
 	{
 		profesoriTable=ProfesoriJTable.getInstance();
+		TableRowSorter<TableModel> sortiranje = new TableRowSorter<TableModel>(profesoriTable.getModel());
+		profesoriTable.setRowSorter(sortiranje);
 		JScrollPane scrollPane = new JScrollPane(profesoriTable); //pravimo scrollPane da bi mogli da skrolujemo ako budemo imali vise redova
 		add(scrollPane,BorderLayout.CENTER);
 	}
